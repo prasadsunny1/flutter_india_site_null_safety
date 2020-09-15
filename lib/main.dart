4660f86d0b7fa1da36901dd57c49556ea0c95529
don-k-jacob <donkjacob.business@gmail.com>
2020-09-16 05:06:59 +0530

    added Timer

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
-
+import 'dart:async';
+import 'package:intl/intl.dart';
 void main() {
   runApp(MyApp());
 }
+Timer _timer;
+int _start = 10;
+
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -11,6 +15,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        backgroundColor: Color(0xff0B1B2B),
+        primaryColor: Color(0xff3889CD),
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
@@ -32,6 +38,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
+  
   MyHomePage({Key key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
@@ -51,7 +58,24 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  int estimateTs = DateTime(2020, 9, 20, 6, 15, 30).millisecondsSinceEpoch; // set needed date
 
+
+void startTimer() {
+  const oneSec = const Duration(seconds: 1);
+  _timer = new Timer.periodic(
+    oneSec,
+    (Timer timer) => setState(
+      () {
+        if (_start < 1) {
+          timer.cancel();
+        } else {
+          _start = _start - 1;
+        }
+      },
+    ),
+  );
+}
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -72,46 +96,22 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      backgroundColor: Colors.black,
+    body: StreamBuilder(
+                      stream: Stream.periodic(Duration(seconds: 1), (i) => i),
+                      builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+                        DateFormat format = DateFormat("mm:ss");
+                        int now = DateTime
+                            .now()
+                            .millisecondsSinceEpoch;
+                        Duration remaining = Duration(milliseconds: estimateTs - now);
+                        var dateString = '${(remaining.inDays<10)?"0":""}${remaining.inDays}:${((remaining.inHours -(remaining.inDays*24))<10)?"0":""}${remaining.inHours -(remaining.inDays*24)}:${format.format(
+                            DateTime.fromMillisecondsSinceEpoch(remaining.inMilliseconds))}';
+                        print(dateString);
+                        return Container(color: Color(0xff0B1B2B),
+                          alignment: Alignment.center,
+                          child: Text(dateString,style: TextStyle(fontSize: 50,color: Colors.white,fontWeight: FontWeight.bold),),);
+                      }),
+  );
   }
 }
